8825174ed63ee6610f4ac65da8411c22de698a7d
KrishnenduDG <krishnendu.kdg@gmail.com>
2025-05-09 20:54:11 +0530

    minor fix

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import fs from "fs";
 import { PORT } from "./config.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ status: true, msg: "Server is up and running!" });
+app.get("/health", (req, res) => {
+  const version = JSON.parse(fs.readFileSync("package.json"))["version"];
+
+  res.json({ status: true, msg: "Server is up and running!", version });
 });
 
 app.listen(PORT, () =>
